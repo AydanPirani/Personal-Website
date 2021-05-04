5b36ae7ccdffcceb6e89ff1121c863acc1d30565
Aydan Pirani <aydanpirani@gmail.com>
2021-05-04 15:57:21 -0700

    Saved.

--- src/Components/ModalContent.jsx
+++ src/Components/ModalContent.jsx
@@ -8,29 +8,29 @@ import { SiAutodesk, SiFirebase, SiFlask, SiJavascript } from "react-icons/si"
 
 function ModalContent() {
     return(
-    <div style = {{border: "2px solid #000", padding: "50px", background: "#e6edecCC", }}>
-            <h2>SKILLS/TECHNOLOGIES</h2>
-        <Box style={{display: "flex", alignItems: "center"}}> 
-            <h3> Languages: </h3>
-            <DiPython size="50px"style={{padding: "20px"}}/>
-            <DiJava size="50px"style={{padding: "20px"}}/>
-            <SiJavascript size="50px"style={{padding: "20px"}}/>
-        </Box>
-        <Box style={{display: "flex", alignItems: "center"}}> 
-            <h3> Frontend: </h3>
-            <AiFillHtml5 size="50px"style={{padding: "20px"}}/>
-            <DiCss3 size="50px"style={{padding: "20px"}}/>
-            <DiReact size="50px" style={{padding: "20px"}}/>
-        </Box>
-        <Box style={{display: "flex", alignItems: "center"}}> 
-            <h3> Technologies: </h3>
-            <FaNodeJs size="50px" style={{padding: "20px"}}/>
-            <AiFillGithub size="50px" style={{padding: "20px"}}/>
-            <SiFlask size = "50px" style={{padding: "20px"}}/>
-            <SiFirebase size = "50px" style={{padding: "20px"}}/>
-            <SiAutodesk size = "50px" style={{padding: "20px"}}/>
-        </Box>
-      </div>
+        <div style = {{border: "2px solid #000", padding: "50px", background: "#e6edecCC", }}>
+                <h2>SKILLS/TECHNOLOGIES</h2>
+            <Box style={{display: "flex", alignItems: "center"}}> 
+                <h3> Languages: </h3>
+                <DiPython size="50px"style={{padding: "20px"}}/>
+                <DiJava size="50px"style={{padding: "20px"}}/>
+                <SiJavascript size="50px"style={{padding: "20px"}}/>
+            </Box>
+            <Box style={{display: "flex", alignItems: "center"}}> 
+                <h3> Frontend: </h3>
+                <AiFillHtml5 size="50px"style={{padding: "20px"}}/>
+                <DiCss3 size="50px"style={{padding: "20px"}}/>
+                <DiReact size="50px" style={{padding: "20px"}}/>
+            </Box>
+            <Box style={{display: "flex", alignItems: "center"}}> 
+                <h3> Technologies: </h3>
+                <FaNodeJs size="50px" style={{padding: "20px"}}/>
+                <AiFillGithub size="50px" style={{padding: "20px"}}/>
+                <SiFlask size = "50px" style={{padding: "20px"}}/>
+                <SiFirebase size = "50px" style={{padding: "20px"}}/>
+                <SiAutodesk size = "50px" style={{padding: "20px"}}/>
+            </Box>
+        </div>
     );
 }
 
